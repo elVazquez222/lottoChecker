412f533afade505699fe90a00282868c3bdf6ce9
Jose Guerrero Vazquez <jbmj@outlook.de>
2021-02-24 08:44:48 +0100

    cleanup code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
 
   const [lottoziehungInput, setLottoziehungInput] =   useState([])
   const [lottoziehung, setLottoziehung]           =   useState([])
-  const [lottozahlen, setLottozahlen]             =   useState([1,2,5,11,19,36])
   const a = [ // 6 aus 49
               [1,2,5,11,19,36],
               [5,6,33,39,44,45],
@@ -20,34 +19,16 @@ function App() {
               [23,26,27,34,48,49],
               [15,20,22,23,30,38],
             ]
-  // const a = [ // 6 aus 49
-  //   [14,16,26,34,44],
-  //   [13,17,28,36,44],
-  //   [11,12,23,32,48],
-  //   [3,29,35,43,49],
-  //   [17,18,24,29,36],
-  //   [6,13,19,25,36],
-  //   [11,21,39,44,45],
-  //   [1,21,27,29,39],
-  //   [4,10,27,31,49],
-  //   [6,9,35,37,48],
-  //   [8,14,16,27,35],
-  //   [3,12,22,33,45],
-  //   [6,8,10,16,47],
-  //   [4,5,29,33,40],
-  //   [10,11,21,35,49],
-  //   [5,18,26,36,40],
-  //   [30,31,37,43,44],
-  //   [22,34,36,39,47],
-  // ]
   
 
   function handleChangeInput(e){
     	setLottoziehungInput(e.target.value)
   }
   function handleSubmit(event){
+
     let zahl = Number(event.target[0].value)
     event.preventDefault()
+
     setLottoziehung(prev => [...prev, zahl])
     setLottoziehungInput("")
   }
@@ -68,7 +49,7 @@ function App() {
       <div className="ziehung">
          {
           lottoziehung.map(zahl =>{
-            // console.log(zahl)
+
             return(
               <span key={zahl} className="gezogen" >{zahl}</span>
             )
